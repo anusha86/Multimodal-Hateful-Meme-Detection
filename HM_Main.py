0ce2f5045950424fb0e139e7ebd81be1c01c7cbd
Anusha Chhabra <106374480+anusha86@users.noreply.github.com>
2024-08-21 12:42:56 +0530

    Add files via upload

--- HM_Main.py
+++ HM_Main.py
@@ -1,37 +1,4 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "57157507",
-   "metadata": {
-    "scrolled": false
-   },
-   "outputs": [],
-   "source": [
-    "import os\n",
-    "import pandas as pd\n",
-    "\n",
-    "import torch\n",
-    "from torch.nn.utils.rnn import pad_sequence\n",
-    "from torch.utils.data import DataLoader, Dataset\n",
-    "from PIL import Image\n",
-    "import torch.nn as nn\n",
-    "import torch.optim as optim\n",
-    "import torchvision.models as models\n",
-    "from datetime import datetime\n",
-    "import torch.optim.lr_scheduler as scheduler\n",
-    "import json\n",
-    "import torchvision.transforms as transforms\n",
-    "import random\n",
-    "\n",
-    "from transformers import DistilBertForSequenceClassification\n",
-    "from transformers import DistilBertTokenizerFast\n",
-    "from timm.models.sknet import HVT50_32x4d\n",
-    "\n",
-    "from model.op.TripletOp import TripletOp"
-   ]
-  },
+
   {
    "cell_type": "code",
    "execution_count": 2,
